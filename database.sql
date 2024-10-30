7d83ecfbb93e4b7fdeca6f2d6b88504f3c8dc7df
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-30 18:28:11 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,33 @@
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sys                |
+| test               |
+| world              |
++--------------------+
+7 rows in set (0.01 sec)
+
+mysql> CREATE DATABASE sajin;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sajin              |
+| sakila             |
+| sys                |
+| test               |
+| world              |
++--------------------+
+8 rows in set (0.00 sec)
+
+mysql>  
